45eee229ab885701b1f3b59f7a1a9d609d00f426
Ashis Kumar Joy <ashis263@gmail.com>
2025-01-14 08:32:02 +0600

    added app template

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,11 @@
+import 'animate.css';
+
+const MainLayout = () => {
+    return (
+        <div className='animate__animated animate__fadeIn'>
+            mainlayout
+        </div>
+    );
+}
+
+export default MainLayout;
