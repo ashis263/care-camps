1fc1403f6f280c1448542e9c9600138e1c45fb5b
Ashis Kumar Joy <ashis263@gmail.com>
2025-01-22 09:45:25 +0600

    fixed responsiveness

--- src/pages/AvailableCamps/AvailableCamps.jsx
+++ src/pages/AvailableCamps/AvailableCamps.jsx
@@ -37,28 +37,30 @@ const AvailableCamps = () => {
                         isLayoutToggled ? <RiLayoutGrid2Fill /> : <RiLayoutGridFill />
                     }
                 </div>
-                <label className="input input-sm input-bordered flex items-center gap-2">
-                    <input onChange={handleSearch} type="text" className="grow" placeholder="Search" />
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 16 16"
-                        fill="currentColor"
-                        className="h-4 w-4 opacity-70">
-                        <path
-                            fillRule="evenodd"
-                            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                            clipRule="evenodd" />
-                    </svg>
-                </label>
-                <select onChange={(e) => {
-                    setSortBy(e.target.value);
-                    refetch();
-                }} className="select select-sm select-bordered max-w-xs">
-                    <option disabled selected>Sort by</option>
-                    <option value="name">Name</option>
-                    <option value="fees">Fees</option>
-                    <option value="participantCount">Most registered</option>
-                </select>
+                <div className="flex max-sm:flex-col gap-5">
+                    <label className="input input-sm input-bordered flex items-center gap-2">
+                        <input onChange={handleSearch} type="text" className="grow" placeholder="Search" />
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 16 16"
+                            fill="currentColor"
+                            className="h-4 w-4 opacity-70">
+                            <path
+                                fillRule="evenodd"
+                                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                                clipRule="evenodd" />
+                        </svg>
+                    </label>
+                    <select onChange={(e) => {
+                        setSortBy(e.target.value);
+                        refetch();
+                    }} className="select select-sm select-bordered max-w-xs">
+                        <option disabled selected>Sort by</option>
+                        <option value="name">Name</option>
+                        <option value="fees">Fees</option>
+                        <option value="participantCount">Most registered</option>
+                    </select>
+                </div>
             </div>
             {
                 isPending
